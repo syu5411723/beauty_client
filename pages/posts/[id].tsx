3c55f34bbbb6296421760d941cb644128270d0d9
maekawa-syuki <syu54117123>
2021-10-01 20:31:32 +0900

    data

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -5,6 +5,7 @@ import { GetStaticProps, GetStaticPaths } from 'next'
 
 import { Layout } from '../../components/Layout/Layout'
 import { PostData } from '../../components/data/PostData'
+import { Post } from '../../components/organisms/Post'
 
 type PathPramas = {
     id: string
@@ -16,19 +17,18 @@ type PageProps = {
 export const getStaticPaths= async () => {
     const paths = PostData.map(post => ({
         params:{
-            slug: post.slug,
+            id: post.id,
         }
     }))
     return { paths, fallback: false }
 }
 export const getStaticProps = async ({params}) => {
-    const getPostData = (slug) => {
+    const getPostData = (id) => {
         return {
-            slug,
-            ...PostData,
+            id,
         }
     }
-    const post = getPostData(params.slug)
+    const post = getPostData(params.id)
     return {
         props: {
             post
@@ -36,10 +36,12 @@ export const getStaticProps = async ({params}) => {
     }
 }
 const Posts = ({ post }) => {
+    console.log(post.id)
     return (
         <>
             <Layout>
-                <p>{}</p>
+                {PostData[`${post.id}`].slug}
+                {post.id}
             </Layout>
         </>
     )
